90382a5cb0becd7e2faa0e6fa65f13483fe78a24
AK <ahhingarajiya@gmail.com>
2022-03-30 11:58:36 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "azurerm" {
+  features {}
+}
+
+module "func" {
+  source = "./modules/function_app"
+}
+
+# module "func1" {
+#   source = "./modules/function_app"
+#   azurerm_function_app = "module-fa1"
+#   azurerm_storage_account = "funstoreacc1"
+#   azurerm_app_service_plan = "spfa2"
+# }
+
+# module "func2" {
+#   source = "./modules/function_app"
+#   azurerm_function_app = "module-fa2"
+#   #azurerm_storage_account = "funstoreacc1"
+#   #azurerm_app_service_plan = "spfa2"
+# }
